e87f1ba1fb69a36340ae1d2403c30744dcb84173
jrichardsz <jrichardsz.java@gmail.com>
2020-08-23 17:42:38 -0500

    success AllForOne, todo: build command, logger, use in more complex examples

--- src/org/linkstartjs/webpack/loader/EntrypointModuleCreator.js
+++ src/org/linkstartjs/webpack/loader/EntrypointModuleCreator.js
@@ -15,7 +15,7 @@ function EntrypointModuleCreator() {
   var entrypointTemplate = fileUtils.readFileSync(entrypointTemplatePath, 'utf8');
 
   var headAnnotations = ["DefaultAction", "Page"];
-  var internalAnnotations = ["Autowire","DomElement","Render","ActionListener"];
+  var internalAnnotations = ["Autowire","HtmlElement","Render","ActionListener","HtmlElementsAllForOne"];
   var allAnnotations = headAnnotations.concat(internalAnnotations);
 
   var requireTemplate = `const @dependencyClassName = require('@dependencyLocation');`;
--- src/org/linkstartjs/webpack/loader/LinkstartTemplate.js
+++ src/org/linkstartjs/webpack/loader/LinkstartTemplate.js
@@ -1,5 +1,4 @@
-//import './styles/index.scss'
-// import '/home/jarvis/Github/linkstart-demos/hello_world_render/src/pages/hello_world/index.html'
+import './styles/index.scss'
 
 function LinkStartApplication() {
 
@@ -63,20 +62,20 @@ function LinkStartApplication() {
     } else {
       //search if exist @Render annotation which indicate us that is an alternative
       //to render() method
-      if(typeof action._ls_name === 'undefined' || action._ls_name == null){
+      if (typeof action._ls_name === 'undefined' || action._ls_name == null) {
         console.log("Something is wrong with this action. _ls_name is undefined");
         return;
       }
 
-      if(typeof _this.metaContext[action._ls_name] === 'undefined' || _this.metaContext[action._ls_name] == null){
+      if (typeof _this.metaContext[action._ls_name] === 'undefined' || _this.metaContext[action._ls_name] == null) {
         console.log("Something is wrong with this action. there is not exist in metaContext");
         return;
       }
 
       var variableToUseAsRender = _this.searchAtLeastOneVariableToRender(_this.metaContext[action._ls_name].variables, action._ls_name);
 
-      if(typeof variableToUseAsRender === 'undefined' || variableToUseAsRender == null){
-        console.log(action._ls_name+" action does not have render() method nor @Render annotation");
+      if (typeof variableToUseAsRender === 'undefined' || variableToUseAsRender == null) {
+        console.log(action._ls_name + " action does not have render() method nor @Render annotation");
         return;
       }
 
@@ -84,9 +83,15 @@ function LinkStartApplication() {
       document.getElementById("root").innerHTML = '';
       document.getElementById("root").appendChild(htmlToRender);
 
-      //html dom is ready, bind variables and ActionListeners will be start
-
+      //html dom is ready
+      //bind dom onclick events to action methods
       _this.applyActionListenersAutoBinding(_this.metaContext[action._ls_name].functions, action, action[variableToUseAsRender].getDomElements());
+
+      //bind dom variables to action variables
+      _this.applyDomElemensAutoBinding(action, action[variableToUseAsRender], _this.metaContext[action._ls_name].variables);
+
+      //allForOne
+      _this.bindDomElementAllForOne(action, action[variableToUseAsRender], _this.metaContext[action._ls_name].variables);
     }
   };
 
@@ -103,22 +108,22 @@ function LinkStartApplication() {
   _this.searchAtLeastOneVariableToRender = function(variables, actionName) {
     var renderCount = 0;
     var renderVariableName;
-    for(var i in variables){
-      for(var a in variables[i]){
+    for (var i in variables) {
+      for (var a in variables[i]) {
         var annotation = variables[i][a];
-        if(annotation.name == 'Render'){
-          renderCount ++;
+        if (annotation.name == 'Render') {
+          renderCount++;
           renderVariableName = i;
         }
       }
     }
 
-    if(renderCount == 0){
-      console.log(actionName+" action does not have any @Render annotation");
+    if (renderCount == 0) {
+      console.log(actionName + " action does not have any @Render annotation");
       return;
     }
-    if(renderCount>1){
-      console.log(actionName+" action has more than one @Render annotation. Just one is allowed.");
+    if (renderCount > 1) {
+      console.log(actionName + " action has more than one @Render annotation. Just one is allowed.");
       return;
     }
 
@@ -126,10 +131,10 @@ function LinkStartApplication() {
   }
 
   _this.applyActionListenersAutoBinding = function(functions, action, registeredDomElementsInPage) {
-    for(var internalActionFunctionName in functions){
-      for(var a in functions[internalActionFunctionName]){
+    for (var internalActionFunctionName in functions) {
+      for (var a in functions[internalActionFunctionName]) {
         var annotation = functions[internalActionFunctionName][a];
-        if(annotation.name == 'ActionListener'){
+        if (annotation.name == 'ActionListener') {
           if (typeof action[internalActionFunctionName] !== "undefined" && typeof action[internalActionFunctionName] === "function") {
             var functionInstance = action[internalActionFunctionName];
             var tagNativeId = annotation.arguments.tagId;
@@ -137,18 +142,18 @@ function LinkStartApplication() {
 
             var lsId = _this.searchLinkStartIdInRegisteredDomElements(registeredDomElementsInPage, annotation.arguments.tagId);
 
-            if(typeof lsId === 'undefined'){
+            if (typeof lsId === 'undefined') {
               console.log(`lsId for this element ${annotation.arguments.tagId} is undefined. Did you register the dom element with ls-element=true ?`);
               continue;
             }
             var domElement = _this.getElementByLsId(lsId);
-            if(typeof domElement !== 'undefined'){
-              if(eventType === "onclick"){
+            if (typeof domElement !== 'undefined') {
+              if (eventType === "onclick") {
                 domElement.onclick = functionInstance;
-              }else{
-                console.log("type function not implemented yet: "+eventType);
+              } else {
+                console.log("type function not implemented yet: " + eventType);
               }
-            }else{
+            } else {
               console.log("expected element was not found in html dom");
             }
           }
@@ -157,33 +162,173 @@ function LinkStartApplication() {
     }
   }
 
-  _this.searchLinkStartIdInRegisteredDomElements = function (registeredDomElementsInPage, tagId) {
-    for(var d in registeredDomElementsInPage){
+  _this.applyDomElemensAutoBinding = function(action, page, variables) {
 
-      if(typeof registeredDomElementsInPage[d] === 'undefined'){
+    let domElements = page.getDomElements();
+    var variablesAnnotatedWithDomElement = _this.searchVariablesAnnotatedWith(variables, "HtmlElement");
+
+    variablesAnnotatedWithDomElement.forEach(function(variable, i) {
+
+      var elementDomId = variable.annotationArguments.id;
+      if (typeof elementDomId !== 'undefined') {
+        for (var elementInDom of domElements) {
+          var tagId = elementInDom.tagId;
+          var lsId = elementInDom.lsId;
+          if ((tagId && lsId) && tagId === elementDomId) {
+            action[variable.variableName] = _this.getElementByLsId(lsId);
+          }
+        }
+      } else {
+        console.log(`variable ${variable.variableName} annotated with @DomElement does no have [id] argument`);
+      }
+    });
+  };
+
+  _this.bindDomElementAllForOne = function(action, page, variables) {
+
+    let domElements = page.getDomElements();
+    var variablesAnnotatedWithDomElement = _this.searchVariablesAnnotatedWith(variables, "HtmlElementsAllForOne");
+
+    if(variablesAnnotatedWithDomElement.lenght >1){
+      console.log(`${action._ls_name} has more than one @HtmlElementsAllForOne`);
+      return;
+    }
+
+    var internalActionVariableName = variablesAnnotatedWithDomElement[0].variableName;
+
+    if (typeof internalActionVariableName === 'undefined') {
+      console.log(`HtmlElementsAllForOne annotation was found but, is not possible to determine the related action variable`);
+    }
+
+    //apply binding
+    action[internalActionVariableName] = _this.applyHtmlElementsToSingleVariableBinding(page);
+  };
+
+  _this.applyHtmlElementsToSingleVariableBinding = function(page) {
+    var model = {};
+
+    let modelElements = page.getDomElements();
+    if (typeof modelElements === 'undefined' || modelElements.length == 0) {
+      return;
+    }
+
+    modelElements.forEach(function(modelElement) {
+      let tagId = modelElement.tagId;
+      let lsId = modelElement.lsId;
+
+      if (tagId && lsId) {
+        let domElement = _this.getElementByLsId(lsId);
+        if (domElement.tagName === 'INPUT') {
+
+          domElement.addEventListener("change", function(event) {
+            if(domElement.type === 'text'){
+              model[tagId] = event.target.value;
+            }else if(domElement.type === 'radio'){
+              model[domElement.name] = event.target.value;
+            }else if(domElement.type === 'checkbox'){
+              if(domElement.checked === true){
+                model[tagId] = true;
+              }else{
+                model[tagId] = false;
+              }
+            }
+          });
+          //set default value
+
+          if(domElement.type === 'text'){
+            model[tagId] = domElement.value;
+          }else if(domElement.type === 'radio'){
+            let radioValue = _this.getDefaultRadioValue(domElement);
+            if(typeof radioValue !== 'undefined'){
+              model[domElement.name] = radioValue;
+            }
+          }else if(domElement.type === 'checkbox'){
+            let checkboxValue = _this.getDefaultCheckboxValue(domElement);
+            model[tagId] = checkboxValue;
+          }
+
+        } else if (domElement.tagName === 'SELECT') {
+          domElement.addEventListener("change", function(event) {
+            model[tagId] = event.target.value;
+          });
+          //set default value
+          model[tagId] = _this.getDefaultSelectValue(domElement);
+        } else {
+          console.log("Model binding is not implemented yet for this tag:" + domElement.tagName);
+        }
+      }
+    });
+
+    return model;
+  }
+
+  _this.searchVariablesAnnotatedWith = function(variables, annotationName) {
+    var foundvariables = [];
+    for (var i in variables) {
+      for (var a in variables[i]) {
+        var annotation = variables[i][a];
+        if (annotation.name == annotationName) {
+          foundvariables.push({
+            variableName: i,
+            annotationArguments: annotation.arguments
+          });
+        }
+      }
+    }
+    return foundvariables;
+  }
+
+  _this.searchLinkStartIdInRegisteredDomElements = function(registeredDomElementsInPage, tagId) {
+    for (var d in registeredDomElementsInPage) {
+
+      if (typeof registeredDomElementsInPage[d] === 'undefined') {
         continue;
       }
 
-      if(typeof registeredDomElementsInPage[d].tagId === 'undefined'){
+      if (typeof registeredDomElementsInPage[d].tagId === 'undefined') {
         continue;
       }
 
-      if(registeredDomElementsInPage[d].tagId == tagId){
+      if (registeredDomElementsInPage[d].tagId == tagId) {
         return registeredDomElementsInPage[d].lsId
       }
     }
   };
 
-  _this.getElementByLsId = function (lsId) {
+  _this.getElementByLsId = function(lsId) {
     let list = document.querySelectorAll('[ls-id="' + lsId + '"]');
     if (list.length == 0) {
       console.log("There are not any element with lsId:" + lsId);
-    }else if (list.length == 1) {
+    } else if (list.length == 1) {
       return list[0];
     } else {
       console.log("There are more than one element with this lsId:" + lsId);
     }
   };
+
+  _this.getDefaultRadioValue = function(radioElement) {
+    if(radioElement.checked === true){
+      return radioElement.value;
+    }
+  }
+
+  _this.getDefaultCheckboxValue = function(checkboxElement) {
+    if(checkboxElement.checked === true){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  _this.getDefaultSelectValue = function(selectElement) {
+    var myOptions = selectElement.options;
+    for (var i = 0; i < myOptions.length; i++) {
+      var isDefSelected = myOptions[i].selected;
+      if (isDefSelected) {
+        return myOptions[i].value;
+      }
+    }
+  }
 
   //TODO: how initialize onclick before dom insertion
 
