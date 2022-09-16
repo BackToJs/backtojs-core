ddd948a3b9049795c4e448c55c17fad56cd1ee0e
jrichardsz <jrichardsz.java@gmail.com>
2022-09-15 20:06:32 -0500

    update dependencies

--- src/main/org/linkstartjs/logger/Logger.js
+++ src/main/org/linkstartjs/logger/Logger.js
@@ -4,20 +4,12 @@ function Logger(){
 
 Logger.debug = function(message){
   if(typeof linkstartJsLogLevel !== 'undefined' && linkstartJsLogLevel==="debug"){
-    if(typeof message === 'object'){
-      console.log(JSON.stringify(message, null, 4));
-    }else{
-      console.log(message);
-    }
+    console.log(message);
   }
 }
 
 Logger.info = function(message){
-  if(typeof message === 'object'){
-    console.log(JSON.stringify(message, null, 4));
-  }else{
-    console.log(message);
-  }
+  console.log(message);
 }
 
 module.exports = Logger;
